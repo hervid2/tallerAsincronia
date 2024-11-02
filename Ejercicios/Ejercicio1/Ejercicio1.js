8e7d95541dee80ed1cc9ca2e0016a94af15a05e0
hervid2 <hdcardonab@gmail.com>
2024-11-01 21:23:23 -0500

    1/11/2024

---
+++ Ejercicios/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,24 @@
+// function imprimirNumeros(desde, hasta) {
+//   if (desde < hasta) {
+//     let interval = setInterval(() => {
+//       if (desde === hasta) {
+//         clearInterval(interval);
+//       } else {
+//         console.log(desde++);
+//       }
+//     }, 1000);
+//   } else {
+//     console.log("El valor inicial debe ser menor que el valor final!");
+//   }
+// }
+// imprimirNumeros(16, 15);
+
+function imprimirNumeros(desde, hasta) {
+  desde = 5;
+  hasta = 20;
+
+  for(let num = desde; num <= hasta; num++){
+    setTimeout()
+  }
+  }
+  imprimirNumeros();
